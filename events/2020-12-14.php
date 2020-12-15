900c84458eea75916b399974c21b0ad86ceeebdb
Gregwar <g.passault@gmail.com>
2020-12-15 13:10:34 +0100

    Retrospectif

--- events/2020-12-14.php
+++ events/2020-12-14.php
@@ -5,14 +5,6 @@ return [
     'title' => "Presentation of CRONE team (IMS)",
     'speakers' => 'Pierre Melchior',
     'date_start' => '13:00',
-    'date_end' => '13:30',
-    'where' => 'Remote',
-    'visio' => 'https://webconf.u-bordeaux.fr/b/gre-2dg-d6u'
-  ],
-  [
-    'title' => 'Presentation of RHOBAN team (LaBRI)',
-    'speakers' => 'Olivier Ly & Grégoire Passault',
-    'date_start' => '13:30',
     'date_end' => '14:00',
     'where' => 'Remote',
     'visio' => 'https://webconf.u-bordeaux.fr/b/gre-2dg-d6u'

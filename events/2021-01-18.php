bf63fca8bda0e5a57c117d6e8bd91e43caff48d5
Gregwar <g.passault@gmail.com>
2021-01-06 18:21:45 +0100

    Rhoban and L3i on 18th

--- events/2021-01-18.php
+++ events/2021-01-18.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
-  [
-    'title' => "Presentation of L3i (La Rochelle University)",
-    'speakers' => 'Arnaud Revel',
-    'date_start' => '13:00',
-    'date_end' => '14:00',
-    'where' => 'Remote',
-    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
-  ]
+    [
+        'title' => "Presentation of RHOBAN (University of Bordeaux)",
+        'speakers' => 'Olivier Ly and Grégoire Passault',
+        'date_start' => '13:00',
+        'date_end' => '13:30',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    ],
+    [
+        'title' => "Presentation of L3i (La Rochelle University)",
+        'speakers' => 'Arnaud Revel',
+        'date_start' => '13:30',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    ]
 ];

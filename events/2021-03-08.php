67f4f98d6a152e2ac89f4aa676e2dd975c3219de
Gregwar <g.passault@gmail.com>
2021-01-12 11:15:13 +0100

    Justin Carpentier

---
+++ events/2021-03-08.php
@@ -0,0 +1,12 @@
+<?php
+return
+    [
+        [
+            'title' => "Pinocchio: a universal framework for Robotics and beyond",
+            'speakers' => 'Justin Carpentier',
+            'date_start' => '13:00',
+            'date_end' => '14:00',
+            'where' => 'Remote',
+            'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+        ]
+    ];

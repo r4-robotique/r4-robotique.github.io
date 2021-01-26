c809b28b22e26c7143f5a1214955d6acc8ffc1e8
Gregwar <g.passault@gmail.com>
2021-01-26 13:44:04 +0100

    Sehoon

---
+++ events/2021-03-22.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Learning to Walk via Deep Reinforcement Learning",
+    'speakers' => 'Sehoon Ha (Georgia Institute of Technology)',
+    'date_start' => '15:00',
+    'date_end' => '16:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

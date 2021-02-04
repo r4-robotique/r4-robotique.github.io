09e4da7f28d787ff6b1becc6b79cf7919d831991
Gregwar <g.passault@gmail.com>
2021-02-04 10:34:55 +0100

    Inversion VP / MC

--- events/2021-03-15.php
+++ events/2021-03-15.php
@@ -1,12 +1,12 @@
 <?php
-return
-    [
-        [
-            'title' => 'Presentation of ENSAM/I2M',
-            'speakers' => 'Mehdi Cherif',
-            'date_start' => '13:00',
-            'date_end' => '14:00',
-            'where' => 'Remote',
-            'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
-        ]
-    ];
+
+return [
+  [
+    'title' => "Introduction to quadratic programming for control in robotics",
+    'speakers' => 'Vincent Padois (Auctus, INRIA)',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ]
+];

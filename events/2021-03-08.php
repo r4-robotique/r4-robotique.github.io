88f61824f07b40f9b2ef67ae5b2537a8999f99ff
Gregwar <g.passault@gmail.com>
2021-03-08 17:22:55 +0100

    Pinocchio

--- events/2021-03-08.php
+++ events/2021-03-08.php
@@ -7,6 +7,10 @@ return
             'date_start' => '13:00',
             'date_end' => '14:00',
             'where' => 'Remote',
-            'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+            'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+            'files' => [
+                'Site web de Pinocchio' => 'https://stack-of-tasks.github.io/pinocchio/',
+                'Vidéo' => 'https://www.youtube.com/watch?v=PfhPHmH8lbs'
+            ]
         ]
     ];

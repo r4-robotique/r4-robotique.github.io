6dd21431da0330c316d0e73150011f083e468881
Gregwar <g.passault@gmail.com>
2021-04-02 20:40:40 +0200

    Andrea

---
+++ events/2021-05-03.php
@@ -0,0 +1,12 @@
+<?php
+return
+    [
+        [
+            'title' => "Task-Space Inverse Dynamics: a C++ Library for Efficient Whole-Body Control”",
+            'speakers' => 'Andrea Del Prete',
+            'date_start' => '13:00',
+            'date_end' => '14:00',
+            'where' => 'Remote',
+            'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+        ]
+    ];

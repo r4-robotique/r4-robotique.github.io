160a90bf57d331852b3c69307cae1763d53a432a
Gregwar <g.passault@gmail.com>
2021-04-26 13:03:28 +0200

    Maylis Uhart

--- events/2021-04-26.php
+++ events/2021-04-26.php
@@ -3,7 +3,7 @@ return
     [
         [
             'title' => 'Presentation of ESTIA-Recherche',
-            'speakers' => 'Nadine Couture & Vincent Magimel',
+            'speakers' => 'Nadine Couture, Maylis Uhart & Vincent Magimel',
             'date_start' => '13:00',
             'date_end' => '14:00',
             'where' => 'Remote',

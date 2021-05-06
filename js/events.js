ade032120b40a68e0d6cea97846628e8558470a3
Gregwar <g.passault@gmail.com>
2021-05-06 11:22:01 +0200

    Reworking working group presentation

--- js/events.js
+++ js/events.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-    all_passed = $('.passed');
-    k = all_passed.length-1;
+    $('.top-navigation').html($('.bottom-navigation').html());
 
-    $('.passed').hide();
-    $('.show-passed').click(function() {
-        $(all_passed[k]).show();
-        k -= 1;
-        if (k < 0) {
-            $(this).hide();
-        }
+    function update_page() {
+        $('.events-page').hide();
+        $('.events-page-'+events_page).show();
+        $('.events-pagination .change-page').removeClass('active');
+        $('.events-pagination .change-page[rel="'+events_page+'"]').addClass('active');
+    }
+    
+    $('.events-pagination .change-page').click(function() {
+        events_page = $(this).attr('rel');
+        update_page();
+    });
+    $('.events-pagination .previous-page').click(function() {
+        events_page = Math.max(1, events_page-1);
+        update_page();
+    });
+    $('.events-pagination .next-page').click(function() {
+        events_page = Math.min(events_pages, events_page+1);
+        update_page();
     });
 });

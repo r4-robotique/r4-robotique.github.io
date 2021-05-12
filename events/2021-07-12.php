b1d362480d2ad0cc079d23bbc50107be39a878ae
Gregwar <g.passault@gmail.com>
2021-05-12 11:03:16 +0200

    Margot Vulliez

---
+++ events/2021-07-12.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Partager l'activité entre Humains et robots, une approche orientée tâche pour la téléopération haptique",
+    'speakers' => 'Margot Vulliez',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

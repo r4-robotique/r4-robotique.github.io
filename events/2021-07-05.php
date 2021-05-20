37e0a2fd9fae5658a1a791ad96ffb7bf742f4a2e
Gregwar <g.passault@gmail.com>
2021-05-20 18:27:20 +0200

    JBM et ST

---
+++ events/2021-07-05.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Modèles réduits pour la planification de mouvements complexes pour des robots à jambe",
+    'speakers' => 'Steve Tonneau',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

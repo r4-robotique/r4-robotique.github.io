26dbff06e7af6a33e43a1cd06d24be8eec4df28e
Gregwar <g.passault@gmail.com>
2021-06-07 11:35:10 +0200

    Video RF

--- events/2021-05-31.php
+++ events/2021-05-31.php
@@ -7,6 +7,9 @@ return [
         'date_start' => '13:00',
         'date_end' => '14:00',
         'where' => 'Remote',
-        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+        'files' => [
+            'Video' => 'https://www.youtube.com/watch?v=M4VsicRBf_0'
+        ]
     ]
 ];

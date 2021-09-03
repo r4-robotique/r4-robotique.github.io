c646ee06b26d0c8a4d9d53ac419cf3612f54fc50
Gregwar <g.passault@gmail.com>
2021-09-03 12:28:12 +0200

    MV

--- events/2021-10-04.php
+++ events/2021-10-04.php
@@ -3,7 +3,7 @@
 return [
   [
     'title' => "Robocup presentation",
-    'speakers' => 'Peter Stone',
+    'speakers' => 'Manuela Veloso and Peter Stone',
     'date_start' => '14:00',
     'date_end' => '16:00',
     'where' => 'LaBRI Amphi'

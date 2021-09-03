25c59ac1d8d6e7c91c548600a29c77530618c4df
Gregwar <g.passault@gmail.com>
2021-09-03 11:03:36 +0200

    PS 14

--- events/2021-10-04.php
+++ events/2021-10-04.php
@@ -4,8 +4,8 @@ return [
   [
     'title' => "Robocup presentation",
     'speakers' => 'Peter Stone',
-    'date_start' => '13:00',
-    'date_end' => '14:00',
+    'date_start' => '14:00',
+    'date_end' => '16:00',
     'where' => 'LaBRI Amphi'
   ],
 ];

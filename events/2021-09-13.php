e0d39efc4614b75291bd1acd82df595e35815edf
Gregwar <g.passault@gmail.com>
2021-09-13 17:51:11 +0200

    Steve tonneau

--- events/2021-09-13.php
+++ events/2021-09-13.php
@@ -7,6 +7,10 @@ return [
     'date_start' => '13:00',
     'date_end' => '14:00',
     'where' => 'Remote',
-    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    'files' => [
+        'Slides' => 'stonneau.pdf',
+        'Video' => 'https://www.youtube.com/watch?v=bMvkkBP3X0U'
+    ]
   ],
 ];
--- events/2021-10-04.php
+++ events/2021-10-04.php
@@ -6,6 +6,6 @@ return [
     'speakers' => 'Manuela Veloso and Peter Stone',
     'date_start' => '14:00',
     'date_end' => '16:00',
-    'where' => 'LaBRI Amphi'
+    'where' => 'TBD'
   ],
 ];

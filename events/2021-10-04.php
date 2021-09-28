575e6982efb4f0c10d59d736fc3779f864956bfd
Gregwar <g.passault@gmail.com>
2021-09-28 12:20:12 +0200

    Event 4/10

--- events/2021-10-04.php
+++ events/2021-10-04.php
@@ -1,11 +1,25 @@
 <?php
 
 return [
-  [
-    'title' => "RoboCup presentation",
-    'speakers' => 'Manuela Veloso, Peter Stone and Daniele Nardi',
-    'date_start' => '14:00',
-    'date_end' => '16:00',
-    'where' => 'Amphi LaBRI'
-  ],
+    [
+        'title' => "Planning and Behavior Recognition in Dynamic Multi-agent Systems.",
+        'speakers' => 'Manuela',
+        'date_start' => '14:00',
+        'date_end' => '14:40',
+        'where' => 'Amphi LaBRI'
+      ],
+      [
+        'title' => "Grounded Simulation Learning for Sim2Real ",
+        'speakers' => 'Peter Stone',
+        'date_start' => '14:40',
+        'date_end' => '15:20',
+        'where' => 'Amphi LaBRI'
+      ],
+      [
+        'title' => "Game Strategies for Physical Robot Soccer Players",
+        'speakers' => 'Daniele Nardi',
+        'date_start' => '15:00',
+        'date_end' => '16:00',
+        'where' => 'Amphi LaBRI'
+      ],
 ];

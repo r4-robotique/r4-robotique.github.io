a524da5a28b2aa23481a9ef79543b4687b13348f
Gregwar <g.passault@gmail.com>
2021-09-28 12:04:29 +0200

    Sophie Sakka and Abstract

---
+++ events/2021-10-18.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  [
+    'title' => "<em>Association Robots !</em>: un outil d'expérimentation scientifique et de cadrage des usages robotiques en société.",
+    'abstract' => "La robotique sociale connait depuis quelques années un essor spectaculaire. Des entreprises sont initiées pour l'exploiter, des fonds conséquents sont investis. On ne peut que regretter l'instabilité de ces sociétés dont la plupart disparaissent après quelques années voire quelques mois, ainsi que l'absence totale de cadrage des usages au profit de l'exploitation commerciale. Il existe une séparation entre le monde civil qui utilise ces robots et crée des usages, et le monde de la recherche qui lutte pour y avoir accès et réaliser des expériences de terrain. Les données du terrain ne remontent pas jusqu'aux laboratoires, ou sont inutilisables de par leur hétérogénéité, et la plupart des travaux de recherche portent sur des expériences internes, limitées en temps et en nombre, auprès de populations sensibilisées au sujet (d'autres chercheurs par exemple). En 2014 a été fondée l'association Robots !. C'est un outil citoyen dont l'objectif est de mener une réflexion sur les usages et analyser l'impact social associé à ces usages. Il s'agit aussi de pérenniser les expériences prometteuses socialement, en y associant tous les acteurs de la robotique sociale: chercheur, industriels, distributeurs, citoyens, utilisateurs, intégrateurs, journalistes, etc.",
+    'speakers' => 'Sophie Sakka',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

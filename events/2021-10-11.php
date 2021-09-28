8adb76f7ce2d76b08e96ef6ed9bfa4a87c618621
Gregwar <g.passault@gmail.com>
2021-09-28 23:26:30 +0200

    11/10

---
+++ events/2021-10-11.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Humanoid Sensorimotor Model Based on Continual Learning",
+    'speakers' => 'Jesus Franco Roblès',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

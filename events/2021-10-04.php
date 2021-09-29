fc191f5b8763f6ade2c048cfc307f689b24a2020
Gregwar <g.passault@gmail.com>
2021-09-29 15:29:56 +0200

    Visio

--- events/2021-10-04.php
+++ events/2021-10-04.php
@@ -6,20 +6,23 @@ return [
         'speakers' => 'Manuela Veloso',
         'date_start' => '14:00',
         'date_end' => '14:40',
-        'where' => 'Amphi LaBRI'
+        'where' => 'Amphi LaBRI',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
       ],
       [
         'title' => "Grounded Simulation Learning for Sim2Real ",
         'speakers' => 'Peter Stone',
         'date_start' => '14:40',
         'date_end' => '15:20',
-        'where' => 'Amphi LaBRI'
+        'where' => 'Amphi LaBRI',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
       ],
       [
         'title' => "Game Strategies for Physical Robot Soccer Players",
         'speakers' => 'Daniele Nardi',
         'date_start' => '15:00',
         'date_end' => '16:00',
-        'where' => 'Amphi LaBRI'
+        'where' => 'Amphi LaBRI',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
       ],
 ];

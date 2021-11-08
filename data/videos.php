2013fcb9652e9ed8c33b0ad6b58ad4917263d8a0
Gregwar <g.passault@gmail.com>
2021-11-08 08:35:20 +0100

    Videos

---
+++ data/videos.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+    'videos' => [
+        [
+            'team' => 'ESTIA-Recherche',
+            'title' => 'Le labo',
+            'v' => 'kd2BnMqPB4o'
+        ],
+        [
+            'team' => 'MNEMOSYNE/INRIA',
+            'title' => 'Bdx SO Au plus près du cerveau',
+            'v' => 'HGU1sD-8WkY'
+        ],
+        [
+            'team' => 'REMIX/XLIM institute',
+            'title' => 'UAV-related research topics',
+            'v' => ''
+        ],
+        [
+            'team' => 'REMIX/XLIM institute',
+            'title' => 'Ground robotics-related research topics',
+            'v' => ''
+        ],
+        [
+            'team' => 'REMIX/XLIM institute',
+            'title' => 'Human-Cobot Interaction',
+            'v' => ''
+        ],
+        [
+            'team' => 'LIUPPA/UPPA',
+            'title' => 'Le labo',
+            'v' => 'Gic84a-LQOo'
+        ],
+        [
+            'team' => 'ADAXIS',
+            'title' => "The world's first fully 3D printed boat which is both seaworthy and printed in one single piece",
+            'v' => 'pccCQfLP_D0'
+        ],
+        [
+            'team' => 'ADAXIS',
+            'title' => 'Adaptive layer height 3D printing of a perfect sphere',
+            'v' => 'u2I3Zwrt5-0'
+        ],
+        [
+            'team' => 'ADAXIS',
+            'title' => 'Angled and non-planar 3D printing using industrial robotics ',
+            'v' => 'gvd6qo-Hhco'
+        ],
+        [
+            'team' => 'RoBioSS/PPRIME',
+            'title' => 'Robotique Biomécanique Sport et Santé, The New RoBioSS dexterous robot hand : adaptive grasping and inside hand manipulation',
+            'v' => 'A0LVtBf8esM'
+        ],
+        [
+            'team' => 'RHOBAN/LaBRI',
+            'title' => 'Rhoban Football club ',
+            'v' => 'tF0cr0PYjsk'
+        ],
+        [
+            'team' => 'RHOBAN/LaBRI',
+            'title' => 'The BIP-BIP project, Binage Intra-rang',
+            'v' => 'l4sHiCAmqXs'
+        ]
+    ]
+];

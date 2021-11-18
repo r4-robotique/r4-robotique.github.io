79c8d5b8c820441fb5e86dd9cd1c105417c7b639
Gregwar <g.passault@gmail.com>
2021-11-18 11:07:24 +0100

    Carlos

--- events/2021-11-15.php
+++ events/2021-11-15.php
@@ -3,11 +3,14 @@
 return [
   [
     'title' => "Recent progress on Crocoddyl, predictive control and codesign",
-    'speakers' => 'Carlos Mastallis',
+    'speakers' => 'Carlos Mastalli',
     'abstract' => "The Crocoddyl project aims to make advances in optimal control that potentially enable various applications such as predictive control and codesign in robotics. In this talk, I will give an overview of the salient aspects of Crocoddyl, share recent progress on numerical optimal control, and report recent examples on predictive control and codesign for agile manoeuvres in legged robotics.",
     'date_start' => '13:00',
     'date_end' => '14:00',
     'where' => 'Remote',
     'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    'files' => [
+        'video' => 'https://www.youtube.com/watch?v=idBqlC9UkuA'
+    ]
   ],
 ];

8166ca6128b05ee1f27e3951d38e22d71dabc24c
Gregwar <g.passault@gmail.com>
2021-11-22 10:05:21 +0100

    IM

---
+++ events/2021-12-13.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Modélisation et prédiction des trajectoires humaines : vers une interaction pro-active avec le robot TALOS",
+    'speakers' => 'Isabelle Maroger',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

596407f0b8ff7b9510e6ebbd5fb577108082b8a9
Gregwar <g.passault@gmail.com>
2021-11-22 10:08:52 +0100

    MB

---
+++ events/2022-01-31.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Wolfgang-OP: A Robust Humanoid Robot Platform for Research and Competitions",
+    'speakers' => 'Jasper Güldenstein',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

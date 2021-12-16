09d6bd161132fc01648cec9a00b3f1d4294b20f0
Gregwar <g.passault@gmail.com>
2021-12-16 10:49:06 +0100

    RP

---
+++ events/2022-01-10.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Automatic Curriculum Learning for Developmental Machine Learners",
+    'speakers' => 'Rémy Portela',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

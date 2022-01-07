6cd7d2fe1d4dcde15ad27d82ac95a0cc6f44c64e
Gregwar <g.passault@gmail.com>
2022-01-07 11:50:29 +0100

    Video

--- events/2021-11-29.php
+++ events/2021-11-29.php
@@ -10,6 +10,9 @@ Cette modélisation de la motivation pourra être étendue et exploitée dans de
     'date_start' => '13:00',
     'date_end' => '14:00',
     'where' => 'Remote',
-    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    'files' => [
+        'Video' => 'https://www.youtube.com/watch?v=Kiw--pXTKxE'
+    ]
   ],
 ];

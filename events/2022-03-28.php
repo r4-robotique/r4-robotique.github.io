86d285cfdf7519ad7977cea3536e5f8191773c41
Gregwar <g.passault@gmail.com>
2022-02-14 12:52:41 +0100

    Self driving cars

---
+++ events/2022-03-28.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => 'Prediction of Pedestrians and Drivers’ Behaviours and Trajectories in the context of Autonomous Driving',
+    'speakers' => 'Miguel Ángel Sotelo Vázquez',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

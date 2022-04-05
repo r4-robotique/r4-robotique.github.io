85b11c92aaccc12f6f29450de162bc08c7f9d802
Gregwar <g.passault@gmail.com>
2022-04-05 15:28:56 +0200

    Slides and video Charles Lesire

--- events/2022-04-04.php
+++ events/2022-04-04.php
@@ -8,5 +8,9 @@ return [
     'date_end' => '14:00',
     'where' => 'Remote',
     'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    'files' => [
+        'Slides' => 'slides_05_04_22.pdf',
+        'Video' => 'https://www.youtube.com/watch?v=MQ28pxP-Stw'
+    ]
   ],
 ];

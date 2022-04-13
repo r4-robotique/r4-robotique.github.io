260aabeeb19db4120e5e0fc4874fa609133d579e
Gregwar <g.passault@gmail.com>
2022-04-13 09:36:44 +0200

    Events

---
+++ events/2022-05-09.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => 'Learning from bees: UAV guidance and odometry based on optic flow',
+        'speakers' => 'Franck Ruffier',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    ],
+];

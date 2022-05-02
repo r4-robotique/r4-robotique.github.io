7c2f86c0401e743f472528384b4968edc35baefb
Gregwar <g.passault@gmail.com>
2022-05-02 16:30:03 +0200

    AR logs

--- events/2022-05-02.php
+++ events/2022-05-02.php
@@ -2,11 +2,18 @@
 
 return [
     [
-        'title' => 'Apprentissage par renforcement en pratique avec Stable-Baselines3',
+        'title' => 'Reinforcement Learning in practice with Stable-Baselines3',
         'speakers' => 'Antonin Raffin',
         'date_start' => '13:00',
         'date_end' => '14:00',
         'where' => 'Remote',
         'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+        'files' => [
+            'Video' => 'https://www.youtube.com/watch?v=0uTuwBdJyTw',
+            'Github repository' => 'https://github.com/DLR-RM/stable-baselines3',
+            'Documentation' => 'https://stable-baselines3.readthedocs.io/en/master/',
+            'Longer talk' => 'https://rl-vs.github.io/rlvs2021/tips-and-tricks.html',
+            'Longer talk (slides)' => 'https://araffin.github.io/slides/rlvs-sb3-handson/'
+        ]
     ],
 ];

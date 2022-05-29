90430f70667185c2c576dda8ada988670ec05914
Gregwar <g.passault@gmail.com>
2022-05-29 19:03:05 +0200

    AE

---
+++ events/2022-05-30.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => 'Educational Robotics as a Learning Tool',
+        'speakers' => 'Amy Eguchi',
+        'date_start' => '17:30',
+        'date_end' => '18:30',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    ],
+];

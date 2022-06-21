8e93d1b5506e2823f5b55a42ecf39d4d138b8c88
Gregwar <g.passault@gmail.com>
2022-06-21 17:51:16 +0200

    SC medias

---
+++ events/2022-06-20.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'title' => 'Ideas and software for the locomotion of homemade robots',
+        'speakers' => 'Stéphane Caron',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+        'files' => [
+            'GitHub repositories' => 'https://github.com/tasts-robots',
+            'Video' => 'https://www.youtube.com/watch?v=BlLX2JABLK0'
+        ]
+    ],
+];

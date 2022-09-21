985d16976493f84d196b29994707ac4c66eadd28
Gregwar <g.passault@gmail.com>
2022-09-21 15:21:56 +0200

    AP

---
+++ events/2022-2023/2022-11-07.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Robotique bio-inspirée et adaptive: apprentissage, contrôle et matériel",
+    'speakers' => 'Alexandre Pitti',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

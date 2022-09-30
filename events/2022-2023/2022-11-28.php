300e3929d6929384813bc08bce1376e155a6a896
Gregwar <g.passault@gmail.com>
2022-09-30 14:46:35 +0200

    AEG

---
+++ events/2022-2023/2022-11-28.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Soil-Adaptive Excavation Using Reinforcement Learning",
+    'speakers' => 'Pascal Arturo Egli',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

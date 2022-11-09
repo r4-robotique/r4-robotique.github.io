85746baa05447378cae8db4919863dd26c1d5c1a
Gregwar <g.passault@gmail.com>
2022-11-09 15:38:05 +0100

    Ghiles Mostafaoui

---
+++ events/2022-2023/2022-11-14.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Human Intentional and Unintentional Interpersonal coordination in interaction with a humanoid robot",
+    'speakers' => 'Ghiles Mostafaoui ',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

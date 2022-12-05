105abc5ab630b575f470bfffdfb6571df4b835f1
Gregwar <g.passault@gmail.com>
2022-12-05 16:36:40 +0100

    Jan.

---
+++ events/2022-2023/2023-01-09.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Algorithmes et architecture pour le contrôle de l'exploration d'une zone par une flotte de sous-marins autonomes.",
+    'speakers' => 'Antoine Milot',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];
---
+++ events/2022-2023/2023-01-16.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Toward Force Feedback in Model-Predictive Control",
+    'speakers' => 'Sebastien Kleff',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

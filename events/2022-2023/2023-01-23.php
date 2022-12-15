b06a219e201976da9849fa5293f0a83ea244ca3e
Gregwar <g.passault@gmail.com>
2022-12-15 11:47:33 +0100

    Luis rodolfo

---
+++ events/2022-2023/2023-01-23.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Model-based and learning-inspired estimation and control of Unmanned Aircraft Systems",
+    'speakers' => 'Luis Rodolfo GARCIA CARRILLO',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

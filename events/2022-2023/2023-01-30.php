759b2367277adae77fc5766239e7c77943b8ab39
Gregwar <g.passault@gmail.com>
2023-01-16 14:22:40 +0100

    Adrien Escande

---
+++ events/2022-2023/2023-01-30.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Numerical Optimization and Motion Generation in Robotics",
+    'speakers' => 'Adrien Escande',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

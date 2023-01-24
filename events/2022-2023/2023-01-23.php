f4ab88a83ed24337fb4aaec9ee1baab9dc95fa9c
Gregwar <g.passault@gmail.com>
2023-01-24 10:56:09 +0100

    Fix

--- events/2022-2023/2023-01-23.php
+++ events/2022-2023/2023-01-23.php
@@ -8,6 +8,6 @@ return [
     'date_end' => '14:00',
     'where' => 'Remote',
     'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
-    'youtube' => 'k6iKnQuBW80
+    'youtube' => 'k6iKnQuBW80'
   ],
 ];

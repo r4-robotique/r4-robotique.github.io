fb2d1d0065b100019af3dd5614bf93d1f23ea212
Gregwar <g.passault@gmail.com>
2023-03-08 15:00:31 +0100

    JPD

---
+++ events/2022-2023/2023-03-20.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "BIBBIP : imagerie, IA et robotique pour le désherbage de précision.  ",
+    'speakers' => 'Jean-Pierre Da Costa',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

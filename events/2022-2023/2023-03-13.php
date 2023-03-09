d39e5b426305f02fd40b92eaf5101042e2aeb755
Gregwar <g.passault@gmail.com>
2023-03-09 16:56:21 +0100

    Amit Kumar Mishra

---
+++ events/2022-2023/2023-03-13.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Bio-inspired engineering",
+    'speakers' => 'Amit Kumar Mishra',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

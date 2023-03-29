ab491e6143ab73356b8de8422b84d0adfa93432b
Grégoire Passault <g.passault@gmail.com>
2023-03-29 11:08:37 +0200

    SB

---
+++ events/2022-2023/2023-04-24.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Perspectives et Travaux d’ARMEN en Robotique Parallèle",
+    'speakers' => 'Sébastien Briot',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

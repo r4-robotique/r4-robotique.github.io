83105fadafa7eb1235779687e1f259333935b06d
Grégoire Passault <g.passault@gmail.com>
2023-04-06 16:31:58 +0200

    PBW

---
+++ events/2022-2023/2023-05-15.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Une approche mathématique des lois de la robotique d’Isaac Asimov",
+    'speakers' => 'Pierre-Brice Wieber',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

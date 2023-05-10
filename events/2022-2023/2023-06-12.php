77bfc063435e04fa6fe27fc1527e22d1414b5507
Grégoire Passault <g.passault@gmail.com>
2023-05-10 17:29:24 +0200

    Nicolas Torres

---
+++ events/2022-2023/2023-06-12.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  [
+    'title' => "Model Predictive Control for robots adapting their task space motion online",
+    'abstract' => "High-frequency trajectory re-planning via MPC",
+    'speakers' => 'Nicolas Torres',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    'files' => [
+        'Website' => 'https://auctus-team.gitlabpages.inria.fr/publications/2023/preprint_acceleration_mpc/'
+    ]
+  ],
+];

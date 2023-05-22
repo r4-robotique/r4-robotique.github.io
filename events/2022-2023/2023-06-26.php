938864c659054d12422abc2d482310e45ccce394
Grégoire Passault <g.passault@gmail.com>
2023-05-22 22:45:51 +0200

    MN

---
+++ events/2022-2023/2023-06-26.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Artificial Locomotion, Should we LEARN or OPTIMIZE?",
+    'speakers' => 'Maximilien Naveau',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

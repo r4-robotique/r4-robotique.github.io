330594d58881442ca7a3f3777e9710668723a5f2
Gregwar <g.passault@gmail.com>
2023-09-28 15:14:24 +0200

    VB

---
+++ events/2023-2024/2023-11-06.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "A computational approach for optimizing the design of biped robots",
+    'speakers' => 'Virgile Batto',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+  ],
+];

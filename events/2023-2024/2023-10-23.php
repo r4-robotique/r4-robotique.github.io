54564cac9f645757cabc3ef4a63bedc439d7e148
Grégoire Passault <g.passault@gmail.com>
2023-10-10 10:34:41 +0200

    Annonce contrôle robuste de drones à rotors pivotants

---
+++ events/2023-2024/2023-10-23.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Contrôle robuste de drones à rotors pivotants",
+    'speakers' => 'Tariq Zioud',
+    'date_start' => '13:00',
+    'date_end' => '14:00',
+    'where' => 'Remote',
+    'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+  ],
+];

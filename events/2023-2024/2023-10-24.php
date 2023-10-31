d203ac5641df18614faefb7343068e02048fd789
Grégoire Passault <g.passault@gmail.com>
2023-10-31 16:51:34 +0100

    LR and AS

---
+++ events/2023-2024/2023-10-24.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Learning to optimize dynamic behaviors",
+    'speakers' => 'Ludovic Righetti',
+    'date_start' => '10:00',
+    'date_end' => '12:30',
+    'where' => 'Remote',
+    'youtube' => '47yt7rusG90'
+  ],
+];
--- events/2023-2024/2023-11-06.php
+++ events/2023-2024/2023-11-06.php
@@ -7,6 +7,6 @@ return [
     'date_start' => '10:00',
     'date_end' => '12:30',
     'where' => 'Remote',
-    'visio' => 'about:blank',
+    'visio' => 'https://inria.webex.com/inria/j.php?MTID=mc6d221cd3784b9b421aa4dad0990f7ce',
   ],
 ];

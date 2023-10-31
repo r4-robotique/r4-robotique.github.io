d8b569cb9ed191b6039c3b676dd54fd10c44e5d7
Grégoire Passault <g.passault@gmail.com>
2023-10-31 16:38:45 +0100

    Soutenance Antun

---
+++ events/2023-2024/2023-11-06.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  [
+    'title' => "Soutenance de thèse",
+    'speakers' => 'Antun Skuric',
+    'date_start' => '10:00',
+    'date_end' => '12:30',
+    'where' => 'Remote',
+    'visio' => 'about:blank',
+  ],
+];

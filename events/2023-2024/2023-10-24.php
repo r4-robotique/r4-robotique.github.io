b4ff25d005fc2ca7cd1bf0a9dd940d25bc0e6c38
Grégoire Passault <g.passault@gmail.com>
2023-10-31 16:52:31 +0100

    Time

--- events/2023-2024/2023-10-24.php
+++ events/2023-2024/2023-10-24.php
@@ -4,8 +4,8 @@ return [
   [
     'title' => "Learning to optimize dynamic behaviors",
     'speakers' => 'Ludovic Righetti',
-    'date_start' => '10:00',
-    'date_end' => '12:30',
+    'date_start' => '14:00',
+    'date_end' => '15:30',
     'where' => 'Remote',
     'youtube' => '47yt7rusG90'
   ],

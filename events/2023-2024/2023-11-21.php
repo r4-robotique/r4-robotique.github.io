e0bfc01f9959c4a1caac6a72286aa2d0c27ef3bf
Gregwar <g.passault@gmail.com>
2023-11-16 15:21:12 +0100

    CP

---
+++ events/2023-2024/2023-11-21.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    [
+        'title' => "Robotized measurements for geometric and acoustic characterization of unknown structures",
+        'speakers' => 'Caroline Pascal',
+        'date_start' => '10:00',
+        'date_end' => '12:00',
+        'where' => 'Remote',
+        'visio' => 'https://inria.webex.com/inria/j.php?MTID=m2acd1e4af5406d58422edbe9759180b4',
+        'files' => [
+            'Page web' => 'Caroline Pascal'
+        ]
+    ],
+];

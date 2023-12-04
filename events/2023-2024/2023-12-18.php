a758f0c40d25f5fd520a9e16c2eee1cb2ac47e94
Grégoire Passault <g.passault@gmail.com>
2023-12-04 10:10:45 +0100

    AG

---
+++ events/2023-2024/2023-12-18.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Design, analysis and kinematic control of highly redundant serial robotic arms",
+		'speakers' => 'Angelica Ginnante',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

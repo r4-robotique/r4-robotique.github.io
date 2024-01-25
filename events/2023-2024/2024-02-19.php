00bd4d179361aa83fb797aa9158d8bb464e9decb
Grégoire Passault <g.passault@gmail.com>
2024-01-25 11:55:17 +0100

    SC

---
+++ events/2023-2024/2024-02-19.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Rapier, a fast and cross-platform deterministic 2D and 3D physics engine for Rust and JS.",
+		'speakers' => 'Sébastien Crozet',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

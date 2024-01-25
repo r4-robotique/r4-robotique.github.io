ab39eb79f10ac044dbf514d8fd36cf857465eebe
Grégoire Passault <g.passault@gmail.com>
2024-01-25 15:32:09 +0100

    NPG

---
+++ events/2023-2024/2024-04-08.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Making reinforcement learning more robotics-compatible",
+		'speakers' => 'Nicolas Perrin-Gilbert',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

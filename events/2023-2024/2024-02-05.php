2f7b7a66f2507f1457c29a7d02ee8603a6eb65b8
Grégoire Passault <g.passault@gmail.com>
2024-02-01 11:32:06 +0100

    MD

---
+++ events/2023-2024/2024-02-05.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Estimation de force par vision dans les robots continus",
+		'speakers' => "Matyas Diezinger",
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

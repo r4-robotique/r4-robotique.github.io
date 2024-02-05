778543d1f06c470a3f3678b0790561a9e1baebe3
Grégoire Passault <g.passault@gmail.com>
2024-02-05 09:46:36 +0100

    AB

---
+++ events/2023-2024/2024-03-18.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Online Optimization-based Gait Adaptation of Quadruped Robot Locomotion ",
+		'speakers' => 'Angelo Bratta',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

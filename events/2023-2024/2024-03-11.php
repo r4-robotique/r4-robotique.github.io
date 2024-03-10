ffa07d64778e0278c779a5c89c6976d12971a562
Grégoire Passault <g.passault@gmail.com>
2024-03-10 20:59:12 +0100

    FG

---
+++ events/2023-2024/2024-03-11.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	[
+		'title' => "Optimizing legged robots with co-design",
+		"abstract" => "This presentation focuses on the concurrent optimization approach of robotic systems, in particular legged-robots.
+By leveraging at the same time hardware, software, and control algorithms, new possibilities for creating efficient, adaptable, and versatile robotic systems are possible. With co-design, several metrics for task execution and energy efficiency can be improved.  Our bi-level method (combining trajectory optimization and design optmization) enhances the capabilities of robotic systems by integrating power considerations and leads to the automatic discovery of some of the emerging design paradigms in the field of legged robotics.",
+		'speakers' => 'Fadini Gabriele',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

f5e38bdcae9cf25f032973ef7a48738294fd7220
Grégoire Passault <g.passault@gmail.com>
2024-04-08 14:55:56 +0200

    Pierre Puchaud

---
+++ events/2023-2024/2024-04-10.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "The interplay of fatigue dynamics and task achievement using optimal control predictive simulation",
+		'speakers' => 'Pierre Puchaud',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

e235da813d75d8c6b51781d3c9b629cf6ab54a02
Grégoire Passault <g.passault@gmail.com>
2024-04-29 22:27:41 +0200

    JK

---
+++ events/2023-2024/2024-05-06.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Language conditioned robot policy learning with interactive learning and reinforcement learning.",
+		'speakers' => 'Zlatan Ajanovic, Jelle Luijkx, Runyu Ma, Jens Kober',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

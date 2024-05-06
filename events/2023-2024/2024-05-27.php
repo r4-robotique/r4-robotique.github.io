51d829aceb97899e6323ab996b5dbe4643222226
Grégoire Passault <g.passault@gmail.com>
2024-05-06 09:56:01 +0200

    VF

---
+++ events/2023-2024/2024-05-27.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'title' => "Towards Retail Stores Automation: 6-DOF Pose Estimation combining Deep Learning Object Detection and Dense Depth Alignment",
+		'speakers' => 'irgile Foussereau',
+		'date_start' => '13:00',
+		'date_end' => '14:00',
+		'where' => 'Remote',
+		'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+	],
+];

ed4dbc9f6f3a810b6ee031d58b61c5917053d73c
Grégoire Passault <g.passault@gmail.com>
2024-06-28 15:09:46 +0200

    AUCTUS summer events

---
+++ events/2023-2024/2024-07-10.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	[
+		'title' => "Robotics education and research at the University of Belgrade",
+		'speakers' => 'Kosta Jovanovic',
+        'abstract' => 'The presentation will provide insights into robotics education and research at the University of Belgrade - School of Electrical Engineering (ETF) as one of the largest engineering higher education institutions in Serbia and the Balkan region. The ongoing research of the ETF Robotics group that focuses on applied human-centered robotics will be presented, including the development of neuroergonomics workstations, algorithms for Cartesian stiffness shaping, robot stiffness estimation, human motion analysis and prediction',
+		// 'date_start' => '15:30',
+		// 'date_end' => '16:00',
+		'where' => 'ENSC, salle N208',
+        'speakers_url' => 'https://fornextcobot.etf.bg.ac.rs/sr/naslovna/staff/ph-d-kosta-jovanovic/'
+	],
+];
---
+++ events/2023-2024/2024-07-24.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	[
+		'title' => "Legged locomotion",
+		'speakers' => 'Majid Khadiv',
+        'abstract' => 'The past few years has witnessed a significant progress in the field of legged locomotion. This is mainly due to the availability of high-performance torque-controlled platforms as well as development of algorithms that scale to high-dimensional, hybrid and under-actuated systems. In this talk, I will present my recent research efforts mainly on the algorithmic side, with an emphasis on developing efficient predictive controllers that can be complemented with supervised/reinforcement learning for real-time execution in the real world. I will also share my perspective on the open problems that we still need to solve to have functional humanoid robots in the real world',
+		// 'date_start' => '15:30',
+		// 'date_end' => '16:00',
+		'where' => 'ENSC, salle N208',
+        'speakers_url' => 'https://www.professoren.tum.de/en/khadiv-majid',
+	],
+];

630b90d80fa1c7c1b15ae3e86f9179eafbe3d91d
Grégoire Passault <g.passault@gmail.com>
2024-07-03 11:00:46 +0200

    Horaires séminaires Auctus

--- events/2023-2024/2024-07-24.php
+++ events/2023-2024/2024-07-24.php
@@ -5,9 +5,10 @@ return [
 		'title' => "Legged locomotion",
 		'speakers' => 'Majid Khadiv',
         'abstract' => 'The past few years has witnessed a significant progress in the field of legged locomotion. This is mainly due to the availability of high-performance torque-controlled platforms as well as development of algorithms that scale to high-dimensional, hybrid and under-actuated systems. In this talk, I will present my recent research efforts mainly on the algorithmic side, with an emphasis on developing efficient predictive controllers that can be complemented with supervised/reinforcement learning for real-time execution in the real world. I will also share my perspective on the open problems that we still need to solve to have functional humanoid robots in the real world',
-		// 'date_start' => '15:30',
-		// 'date_end' => '16:00',
+		'date_start' => '11:00',
+		'date_end' => '12:30',
 		'where' => 'ENSC, salle N208',
         'speakers_url' => 'https://www.professoren.tum.de/en/khadiv-majid',
+        'visio' => 'https://inria.webex.com/meet/vincent.padois'
 	],
 ];

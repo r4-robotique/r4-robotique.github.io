b5d942f651977d3f360fbc48558dc43f0d7c7540
Grégoire Passault <g.passault@gmail.com>
2024-10-10 10:01:29 +0200

    Ajay

---
+++ events/2024-2025/2024-11-11.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    [
+        'title' => "Low-order algorithms for efficient robot simulation and control",
+        'speakers' => 'Ajay Sathya',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    ],
+];

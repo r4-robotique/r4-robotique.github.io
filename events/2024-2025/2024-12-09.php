2bbd50f86c9a334d7ddd73b79260eb616d5170e7
Grégoire Passault <g.passault@gmail.com>
2024-10-15 10:41:46 +0200

    LM

---
+++ events/2024-2025/2024-12-09.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => "Contribution à la conception et la fabrication de robots souples pneumatiques",
+        'speakers' => 'Loïc Mosser',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    ],
+];

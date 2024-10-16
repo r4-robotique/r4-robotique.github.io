ad7e8310a31cf4fc6f1ac648eaa379c358d389f0
Grégoire Passault <g.passault@gmail.com>
2024-10-16 22:27:52 +0200

    OL

---
+++ events/2024-2025/2024-10-21.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => "Robotized Additive Manufacturing for Construction Using Recycled Materials in Real-World Environments",
+        'speakers' => 'Othman Lakhal',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567'
+    ],
+];

78696d9bc3fdcdc9605d31332592833015092971
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2025-09-11 21:43:15 +0200

    Add files via upload
    first event

---
+++ events/2025-2026/2025-09-03.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    [
+        'title' => "Applied Robotics for Biomechanics",
+        'speakers' => 'Florent Moissenet',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://u-bordeaux-fr.zoom.us/j/83483228567',
+    ],
+];
+

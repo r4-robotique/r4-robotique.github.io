df42293605553ff51b2406413612c858be804e04
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2025-09-12 19:45:47 +0200

    Correct title for robotics optimization method

--- events/2025-2026/2025-10-13.php
+++ events/2025-2026/2025-10-13.php
@@ -2,7 +2,7 @@
 
 return [
     [
-        'title' => "Method d'optimisation d'ordre zero pour la robotique",
+        'title' => "Methodes d'optimisation d'ordre zero pour la robotique",
         'speakers' => 'Armand Jordana',
         'date_start' => '13:00',
         'date_end' => '14:00',

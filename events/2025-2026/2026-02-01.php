230d812121c97458250c828e502101961197627b
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2025-11-24 18:08:42 +0100

    Add event details for February 1, 2026

---
+++ events/2025-2026/2026-02-01.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => "TBA",
+        'speakers' => 'Xavier Bonnet & Hélène Pillet',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://rendez-vous.renater.fr/r4-robotique',
+    ],
+];

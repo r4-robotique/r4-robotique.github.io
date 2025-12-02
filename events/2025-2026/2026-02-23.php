b570f44e78b8c7732f2fd283626ee55a6f6405ed
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2025-12-02 19:15:16 +0100

    Add event for Brain-inspired Control Scheme talk

---
+++ events/2025-2026/2026-02-23.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [ 
+  [ 
+  'title' => "Brain-inspired Control Scheme for Heterogenous Multi-Agent Systems", 
+  'speakers' => 'Juan Antonio ESCARENO CASTRO', 
+  'date_start' => '13:00', 
+  'date_end' => '14:00', 
+  'where' => 'Remote', 
+  'visio' => 'https://rendez-vous.renater.fr/r4-robotique', 
+  ], 
+];

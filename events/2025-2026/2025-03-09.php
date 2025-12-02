ece27bbeb63ad58dc85a37c0f4f13e820ec1ebe5
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2025-12-02 14:58:06 +0100

    Create 2025-03-09.php

---
+++ events/2025-2026/2025-03-09.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => "TBA",
+        'speakers' => 'Melya Boukheddimi',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://rendez-vous.renater.fr/r4-robotique',
+    ],
+];

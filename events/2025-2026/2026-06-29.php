a103cd3d9c0cc8a81e7d229fb0aa9cea67f2ea09
Pierre Puchaud <40755537+Ipuch@users.noreply.github.com>
2026-06-29 10:02:04 +0200

    Update 2026-06-29.php

--- events/2025-2026/2026-06-29.php
+++ events/2025-2026/2026-06-29.php
@@ -7,6 +7,6 @@ return [
         'date_start' => '13:00',
         'date_end' => '14:00',
         'where' => 'Remote',
-        'visio' => 'https://rendez-vous.renater.fr/r4-robotique',
+        'visio' => 'https://visio.numerique.gouv.fr/jbk-zhiz-vlm',
     ],
 ];

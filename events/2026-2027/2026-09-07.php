7a1f9f4174b661899bcfaf9ee5e9523cdac993b0
ipuch <pierre.puchaud@inria.fr>
2026-07-07 17:24:08 +0200

    feat: kimodo

---
+++ events/2026-2027/2026-09-07.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    [
+        'title' => "Kimodo: Scaling Controllable Human Motion Generation",
+        'speakers' => 'Mathis Petrovich (NVDIA)',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://visio.numerique.gouv.fr/jbk-zhiz-vlm',
+    ],
+];
+

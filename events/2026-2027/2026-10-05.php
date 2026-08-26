49eafb9500eca96ac4f0fedb0f5103fa2ec5c259
ipuch <pierre.puchaud@inria.fr>
2026-08-26 14:57:18 +0200

    go

---
+++ events/2026-2027/2026-10-05.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => "TBD",
+        'speakers' => 'Clara Beatriz Sanz-Morère (Computación y tecnología, Escuela de Arquitectura, Ingeniería, Ciencias y Computación, Universidad Europea de Madrid)',
+        'date_start' => '13:00',
+        'date_end' => '14:00',
+        'where' => 'Remote',
+        'visio' => 'https://visio.numerique.gouv.fr/jbk-zhiz-vlm',
+    ],
+];
